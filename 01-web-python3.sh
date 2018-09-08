daab6a0d1f42a4198d2e4378e3041de3ee762629
Mario Medeiros <m4r10m@gmail.com>
2018-09-08 19:55:39 -0300

    Add files via upload

---
+++ 01-web-python3.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+#Script para modulo simple http server do python 3.5.2
+#Habilitando o servidor para compartilhamento de arquivos
+#Com opcao de escolha de diretorios do usuario corrente
+#
+#Autor: Mario Medeiros
+#Site: www.mariomedeiros.eti.br
+#Data: 2018-09-05
+#Versao: 0.1-Z
+#############################################################
+
+dir=$(zenity --file-selection --directory)
+
+pushd "$dir";
+python3.5m -m http.server | zenity --progress --text="WebServer Python Habilitado $dir" --pulsate --auto-kill &
+popd;
+
+(RUNNING=0
+while [ $RUNNING -eq 0 ]
+do
+if [ -z "$(pidof zenity)" ]
+    then
+    pkill -9 python
+    RUNNING=1
+fi
+done
+)
